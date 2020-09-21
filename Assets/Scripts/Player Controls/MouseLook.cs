9adc2ab62ce48a51f2e945536cadc5fda886c208
Samir Georgy <57589276+samirgeorgy@users.noreply.github.com>
2020-09-21 19:40:53 +0200

    Player Basic Movement Implemented, Integrated Filebase and downloaded some models

---
+++ Assets/Scripts/Player Controls/MouseLook.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/Mouse Look")]
+public class MouseLook : MonoBehaviour
+{
+    #region Private Variables
+
+    private enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+
+    [SerializeField] private RotationAxes axes = RotationAxes.MouseXAndY;
+    [SerializeField] private float sensitivityX = 15F;
+    [SerializeField] private float sensitivityY = 15F;
+
+    
+    private float minimumX = -360F;
+    private float maximumX = 360F;
+    private float minimumY = -60F;
+    private float maximumY = 60F;
+    private float rotationY = 0F;
+
+    #endregion
+
+    #region Unity Functions
+
+    void Update()
+    {
+
+        if (axes == RotationAxes.MouseXAndY)
+        {
+            float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+        }
+        else if (axes == RotationAxes.MouseX)
+        {
+            transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+        }
+        else
+        {
+            rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+            rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+            transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+        }
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/Player Controls/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    #region Private Variables
+
+    [SerializeField] private float _speed = 5.0f;
+    [SerializeField] private float _jumpHeight = 10.0f;
+    [SerializeField] private float _gravity = 1.0f;
+
+    private Vector3 _direction;
+
+    private CharacterController _controller;
+
+    #endregion
+
+    #region Unity Functions
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMovement();
+    }
+
+    #endregion
+
+    #region Supporting Functions
+
+    private void PlayerMovement()
+    {
+        if (_controller.isGrounded)
+        {
+            float horizontalMovement = Input.GetAxis("Horizontal");
+            float verticalMovement = Input.GetAxis("Vertical");
+
+            _direction = (transform.right * horizontalMovement + transform.forward * verticalMovement) * _speed;
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                _direction.y += _jumpHeight;
+            }
+        }
+
+        _direction.y -= _gravity * Time.deltaTime;
+        _controller.Move(_direction * Time.deltaTime);
+    }
+
+    #endregion
+}
